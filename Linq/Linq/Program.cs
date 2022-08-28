920e1f43fb893becab20b6e14e42f4701e90c67a
Hariom Sharma <hsharmaiet@gmail.com>
2022-08-28 09:22:50 -0700

    Initial commit

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Linq;
+
+Console.WriteLine("Hello, World!");
+List<Employee> employees = new List<Employee>
+{
+    new Employee{Name="Hariom"},
+    new Employee{Name="Hariom"},
+    new Employee{Name="Sam"}
+};
+var result  = employees.GroupBy(x => x.Name).Select(x=>x.Count());
+var result1 = employees.GroupBy(x => x.Name).Select(x => x.FirstOrDefault());
+foreach (int i in result) { Console.WriteLine(i);}   
+Console.ReadKey();
